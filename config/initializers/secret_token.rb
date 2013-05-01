586d2f4d30808363ccfc1deacedee6972f7d71c3
George Ezenna <georgeezenna@gmail.com>
2013-05-01 12:47:27 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+George::Application.config.secret_token = '6246e522b461c1a13e246bb6885245b61928273f46786dbf1c2eb8f496f906579d1e7d0cc209f9ae20ce8577586b1afef34d1482aa600b6d624c0682e4ee781c'
